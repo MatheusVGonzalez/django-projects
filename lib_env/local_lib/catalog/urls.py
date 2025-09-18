0036629cf13b2291d8098d66519429fde12c0c2c
Matheus Gonzalez <matheusverissimogonzalez@gmail.com>
2025-09-17 17:40:13 -0700

    Feat: Author details have the books related to the author now

--- lib_env/local_lib/catalog/urls.py
+++ lib_env/local_lib/catalog/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('',Index,name='index'),
     path('books/',BookView.as_view(),name='books'),
     path('books/<int:pk>',BookView.as_view(),name='book_detail'),
-    path('authors/',AuthorView.as_view(),name='authors'),
+    path('authors/',author_list ,name='authors'), 
     path('authors/<int:pk>',AuthorView.as_view(),name='author_detail')
 ]
